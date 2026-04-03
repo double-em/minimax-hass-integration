20ab86b822c7acb5c7454fb82c9779b953619288
double-em <8335996+double-em@users.noreply.github.com>
2026-04-03 09:05:59 +0200

    Fix config flow 400 error and subentry state pollution
    - Remove duplicate CONF_API_KEY import from homeassistant.const
    - Convert class variable last_rendered_recommended to instance variable
      to prevent state pollution across multiple subentry flow instances
    - Fixes 'Config flow could not be loaded: 400: Bad Request' error

--- custom_components/minimax/config_flow.py
+++ custom_components/minimax/config_flow.py
@@ -16,7 +16,6 @@ from homeassistant.config_entries import (
     ConfigSubentryFlow,
     SubentryFlowResult,
 )
-from homeassistant.const import CONF_API_KEY
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.selector import (
@@ -181,7 +180,10 @@ class MiniMaxConfigFlow(ConfigFlow, domain=DOMAIN):
 class LLMSubentryFlowHandler(ConfigSubentryFlow):
     """Flow for managing subentries."""
 
-    last_rendered_recommended = False
+    def __init__(self) -> None:
+        """Initialize the subentry flow handler."""
+        super().__init__()
+        self._last_rendered_recommended = False
 
     @property
     def _is_new(self) -> bool:
@@ -212,10 +214,10 @@ class LLMSubentryFlowHandler(ConfigSubentryFlow):
             else:
                 options = self._get_reconfigure_subentry().data.copy()
                 _LOGGER.debug("Existing subentry, current options: %s", options)
-            self.last_rendered_recommended = bool(options.get(CONF_RECOMMENDED, False))
+            self._last_rendered_recommended = bool(options.get(CONF_RECOMMENDED, False))
         else:
             _LOGGER.debug("User provided input: %s", user_input)
-            if user_input.get(CONF_RECOMMENDED) == self.last_rendered_recommended:
+            if user_input.get(CONF_RECOMMENDED) == self._last_rendered_recommended:
                 if self._is_new:
                     _LOGGER.info("Creating new subentry: %s", self._subentry_type)
                     return self.async_create_entry(
@@ -228,7 +230,7 @@ class LLMSubentryFlowHandler(ConfigSubentryFlow):
                     self._get_reconfigure_subentry(),
                     data=user_input,
                 )
-            self.last_rendered_recommended = user_input.get(CONF_RECOMMENDED, False)
+            self._last_rendered_recommended = user_input.get(CONF_RECOMMENDED, False)
             options = user_input
 
         schema = async_minimax_option_schema(self._is_new, self._subentry_type, options)
